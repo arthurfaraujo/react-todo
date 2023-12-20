2c6d329c4d02cfbfbae67f44366ecb23badb2853
arthurfaraujo <arthurfelipe5567@gmail.com>
2023-12-20 14:12:28 -0300

    feat: add a simple layout

---
+++ src/layout/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <header>
+      <nav className="navBar">
+        <ul className="navList">
+          <li className="navItem">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navItem">
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Layout from './layout'
 
 export const Router = createBrowserRouter([
   {
     path: '/',
+    element: <Layout />,
     children: [
       {
         index: true,
