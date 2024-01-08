7ce808980ba2abcf815fa07d617a586c61bf3265
arthur <arthurfelipe5567@gmail.com>
2024-01-08 11:54:59 -0300

    fix: add checked state to todo
    now it is possible to check and uncheck todos

--- src/pages/Home/Todo.tsx
+++ src/pages/Home/Todo.tsx
@@ -1,14 +1,18 @@
 import { TodoItf } from "../../common/interfaces"
 import { TodoContext } from "../../contexts/Todo"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 
 function Todo({ info }: { info: TodoItf }) {
   const { id, title, completed } = info
   const { handleClick, handleChange } = useContext(TodoContext)
+  const [checked, setChecked] = useState<boolean>(completed)
 
   return <li className="todo">
-    <input type="checkbox" checked={completed} onChange={() => handleChange(id)} />
-    <p className={completed ? 'checked' : ''}>{title}</p>
+    <input type="checkbox" checked={checked} onChange={() => {
+      setChecked(!checked)
+      handleChange(id)
+    }} />
+    <p className={checked ? 'checked' : ''}>{title}</p>
     <button onClick={() => handleClick(id)}>&times;</button>
   </li>
 }
