1ea7cb763d03a0c0cf7aeaf3f26cbd9ce1807bf4
arthur <arthurfelipe5567@gmail.com>
2024-01-15 12:19:59 -0300

    feat: add token verification logic
    now the token is sent to the backend server
    to be verified and then the user is redirected

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,14 +1,40 @@
 import { Navigate } from "react-router-dom"
-import { useContext } from "react"
+import API from "../services/axios"
+import { useContext, useEffect, useState } from "react"
 import { TodoContext } from "../contexts/Todo"
 
 function PrivateRoute({ element }: { element: JSX.Element }) {
-  const { getToken } = useContext(TodoContext)
-  if (getToken()) {
-    return element
-  } else {
-    return <Navigate to="/login" />
-  }
+  const { changeToken } = useContext(TodoContext)
+  const token = localStorage.getItem('@react-todo:token')
+  const [verified, setVerified] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (token) {
+      API.req.get(`/user/verify/${token}`).then(res => {
+        if (res.data.verified) {
+          setLoading(false)
+          setVerified(true)
+          changeToken(token)
+        } else {
+          setLoading(false)
+          setVerified(false)
+          changeToken()
+        }
+      }).catch(() => {
+        setLoading(false)
+        setError(true)
+      })
+    } else {
+      setLoading(false)
+      setVerified(false)
+    }
+  }, [changeToken, token])
+
+  return (
+    loading ? <div>Loading...</div> : error ? <div>Error...</div> : verified ? element : <Navigate to="login" />
+  )
 }
 
 export default PrivateRoute
