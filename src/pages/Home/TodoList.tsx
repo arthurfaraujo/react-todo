95a426fd5a4fe2f0c24475ee185d006c0e43a66d
arthur <arthurfelipe5567@gmail.com>
2024-01-15 12:15:34 -0300

    feat: move todo request to TodoList

--- src/pages/Home/TodoList.tsx
+++ src/pages/Home/TodoList.tsx
@@ -1,16 +1,41 @@
 import Todo from "./Todo"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { TodoContext } from "../../contexts/Todo"
+import API from "../../services/axios"
 
 function List() {
-  const { list } = useContext(TodoContext)
+  const { list, setList } = useContext(TodoContext)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    API.authReq.get('/todo').then(res => {
+      setList(res.data.todos)
+      setLoading(false)
+    }).catch(() => {
+      setLoading(false)
+      setError(true)
+    })
+  }, [setList])
+
+  if (loading) {
+    return <p className="loading">Loading...</p>
+  }
+
+  if (error) {
+    return <p className="error">Error</p>
+  }
 
   return (
-    <ul className="todoList">
-      {list?.map((todo, index) => {
-        return <Todo key={index} info={{ ...todo }} />
-      })}
-    </ul>
+    <>
+      {list.length ?
+        <ul className="todoList">
+          {list.map((todo, index) => {
+            return <Todo key={index} info={{ ...todo }} />
+          })}
+        </ul> :
+        <p className="noTodos">There is no todo...</p>}
+    </>
   )
 }
 
